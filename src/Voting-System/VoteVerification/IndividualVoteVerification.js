879852320b029059876365608811378a09a3a730
Christina Nissen <christinaf.nissen@hotmail.com>
2023-03-16 13:07:13 +0100

    Updated the links to the survey

--- src/Voting-System/VoteVerification/IndividualVoteVerification.js
+++ src/Voting-System/VoteVerification/IndividualVoteVerification.js
@@ -94,7 +94,7 @@ export default function IndividualVoteVerification() {
                       padding={"1rem"}
                       onClick={() =>
                         (window.location.href =
-                          "https://www.survey-xact.dk/LinkCollector?key=TC9S9SFFJPC5")
+                          "https://www.survey-xact.dk/LinkCollector?key=SDWDX5F9S51N")
                       }
                     >
                       Go to survey
--- src/Voting-System/VoteVerification/VoteVerification.js
+++ src/Voting-System/VoteVerification/VoteVerification.js
@@ -285,7 +285,7 @@ export default function VoteVerification() {
                       left={"-19.99"}
                       onClick={() =>
                         (window.location.href =
-                          "https://www.survey-xact.dk/LinkCollector?key=TC9S9SFFJPC5")
+                          "https://www.survey-xact.dk/LinkCollector?key=SDWDX5F9S51N")
                       }
                     >
                       Go to survey
